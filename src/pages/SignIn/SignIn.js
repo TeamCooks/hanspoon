705bd76b4c81a0fef906b64c4c22547c7aee2d51
hanana1253 <hanana1253@gmail.com>
2022-01-11 19:26:17 +0900

    feat: Make Signin and signup component with YUP and Formik

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,3 +1,69 @@
-export function SignIn () {
-  return <div>signIn</div>
-}
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+export const SignIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      email: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(3, 'Must be 3 characters or more')
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Must be 8 characters or more')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="username">username</label>
+      <input
+        id="username"
+        name="username"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.username}
+      />
+      {formik.touched.username && formik.errors.username ? (
+        <div>{formik.errors.username}</div>
+      ) : null}
+
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,3 +1,85 @@
-export function SignUp () {
-  return <div>signUppppp</div>
-}
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+export const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      passwordConfirm:'',
+      email: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(3, 'Must be 3 characters or more')
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Must be 8 characters or more')
+        .required('Required'),
+      passwordConfirm: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="username">username</label>
+      <input
+        id="username"
+        name="username"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.username}
+      />
+      {formik.touched.username && formik.errors.username ? (
+        <div>{formik.errors.username}</div>
+      ) : null}
+
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
+      <label htmlFor="passwordConfirm">Password Confirm</label>
+      <input
+        id="passwordConfirm"
+        name="passwordConfirm"
+        type="passwordConfirm"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.passwordConfirm}
+      />
+      {formik.touched.passwordConfirm && formik.errors.passwordConfirm ? (
+        <div>{formik.errors.passwordConfirm}</div>
+      ) : null}
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
