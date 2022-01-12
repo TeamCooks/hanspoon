856fcb1b9e4f202eeee635f7a43533a9fc629572
hanana1253 <hanana1253@gmail.com>
2022-01-12 12:12:05 +0900

    feat: Add authError message when failed auth request in signIn.js
    If user info is incorrect it will show login fail message
    
    TODO: Do the same thing in signup.js, and divide component if possible.

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import { signIn } from '../../api/requestAuth';
 import { signInSchema, FormikField } from '../../services';
 
 export const SignIn = () => {
+  const [hasAuthError, setAuthError] = useState(false);
   const formik = useFormik({
     initialValues: {
       password: '',
@@ -15,18 +16,22 @@ export const SignIn = () => {
       try {
         const user = await signIn(values);
         console.log(user);
+        setAuthError(false);
         // 여기에 auth 정보를 context에 update하기
       } catch (e) {
-        window.alert('API 호출 후 인증 실패 에러메시지를 노출해야합니다.');
+        setAuthError(true);
       }
     },
   });
   return (
+    <>
+    {hasAuthError ? <div className="authError">Login Failed. Please Try Again.</div> : null}
     <form onSubmit={formik.handleSubmit}>
       <FormikField fieldname={'email'} formik={formik} />
       <FormikField fieldname={'password'} formik={formik} />
       <button type="submit">Submit</button>
     </form>
+    </>
   );
 };
 
