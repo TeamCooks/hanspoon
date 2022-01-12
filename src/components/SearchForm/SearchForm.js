25c7f0c9a2b6f4e84b881a611b74a3d5ce31e7b0
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-01-12 21:18:25 +0900

    feat: Add accessibility and Icon

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,16 @@
 import styles from './SearchForm.module.scss';
+import { FiSearch } from 'react-icons/fi';
 
 export function SearchForm() {
   return (
     <form className={styles.searchForm}>
-      <input className={styles.searchInput} />
-      <button className={styles.searchButton} />
+      <label className="a11yHidden" for="searchRecipe">
+        Search recipes
+      </label>
+      <input id="searchRecipe" type="search" className={styles.searchInput} autoComplete="off" />
+      <button className={styles.searchButton} aria-label="search">
+        <FiSearch />
+      </button>
     </form>
   );
 }
