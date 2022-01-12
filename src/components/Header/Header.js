0a24076f65e318af74d0078c47d72f939ac7d0c9
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-01-12 19:58:45 +0900

    chore: header basic styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,14 @@
+import './Header.scss'
+import { Link } from 'react-router-dom';
+
 export function Header() {
-  return <p>Header</p>;
+  return (
+    <header className='header'>
+      <div className='wrapper'>
+        <Link to="/">Han Spoon</Link>
+        <div>검색컴포넌트</div>
+      </div>
+      <div>로그인버튼</div>
+    </header>
+  );
 }
