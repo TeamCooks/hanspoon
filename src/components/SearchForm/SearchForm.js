676f02fdb2e1289e40fbea3417fcf23331136055
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-01-13 16:39:54 +0900

    feat: Implement of search function

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,40 @@
 import styles from './SearchForm.module.scss';
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 export function SearchForm() {
+  const [keyword, setKeyword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setKeyword('');
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+    } else {
+      alert('Please enter the search word.');
+    }
+  };
+
   return (
-    <form className={styles.form}>
-      <label className="a11yHidden" for="searchRecipe">
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className="a11yHidden" htmlFor="searchRecipe">
         Search recipes
       </label>
-      <input id="searchRecipe" type="search" className={styles.input} autoComplete="off" />
+      <input
+        id="searchRecipe"
+        type="search"
+        className={styles.input}
+        autoComplete="off"
+        value={keyword}
+        onChange={handleChange}
+      />
       <button className={styles.button} aria-label="search">
         <FiSearch />
       </button>
