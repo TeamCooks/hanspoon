c99e3cef37bbd4bf80fce0cedee9d892526338bf
joker77z <joker77z@naver.com>
2022-01-15 22:53:14 +0900

    conf: Set config-overrides for webpack
    utils 함수들을 편하게 불러올 수 있도록 적용

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,6 @@ module.exports = override(
   addWebpackAlias({
     '@styles': path.resolve(__dirname, 'src/styles'),
     '@api': path.resolve(__dirname, 'src/api'),
+    '@utils': path.resolve(__dirname, 'src/utils'),
   }),
 );
