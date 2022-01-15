9f37dde0b99aa8917e212657ee3ca90b2eeb0af3
joker77z <joker77z@naver.com>
2022-01-15 23:55:04 +0900

    feat: Add hotrecipes example data
    - 홈 페이지에서 어떻게 레이아웃이 나타나는지 확인하는 용도로 일단 스타일과 예시용 카드 컴포넌트 추가

--- src/components/HotRecipes/HotRecipes.js
+++ src/components/HotRecipes/HotRecipes.js
@@ -1,18 +1,78 @@
 import { Card } from '../Card/Card';
+import styles from './HotRecipes.module.scss';
 
 export function HotRecipes() {
   return (
-    <section>
-      <h2>HotRecipes</h2>
-      <Card
-        type="wide"
-        background="none"
-        summary={false}
-        headingPosition="bottomCenter"
-        imgSrc="http://placehold.it/312x230"
-        foodName="Easy Strawberry Cake"
-        summaryText={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
-      />
+    <section className={styles.section}>
+      <h2 className={styles.contentHeader}>HotRecipes</h2>
+      <ul className={styles.cardItems}>
+        <li>
+          <Card
+            type="wide"
+            background="none"
+            hasSummary={false}
+            headingPosition="bottomCenter"
+            imgSrc="http://placehold.it/312x230"
+            title="Easy Strawberry Cake"
+            summary={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
+          />
+        </li>
+        <li>
+          <Card
+            type="wide"
+            background="none"
+            hasSummary={false}
+            headingPosition="bottomCenter"
+            imgSrc="http://placehold.it/312x230"
+            title="Easy Strawberry Cake"
+            summary={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
+          />
+        </li>
+        <li>
+          <Card
+            type="wide"
+            background="none"
+            hasSummary={false}
+            headingPosition="bottomCenter"
+            imgSrc="http://placehold.it/312x230"
+            title="Easy Strawberry Cake"
+            summary={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
+          />
+        </li>
+        <li>
+          <Card
+            type="wide"
+            background="none"
+            hasSummary={false}
+            headingPosition="bottomCenter"
+            imgSrc="http://placehold.it/312x230"
+            title="Easy Strawberry Cake"
+            summary={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
+          />
+        </li>
+        <li>
+          <Card
+            type="wide"
+            background="none"
+            hasSummary={false}
+            headingPosition="bottomCenter"
+            imgSrc="http://placehold.it/312x230"
+            title="Easy Strawberry Cake"
+            summary={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
+          />
+        </li>
+        <li>
+          <Card
+            type="wide"
+            background="none"
+            hasSummary={false}
+            headingPosition="bottomCenter"
+            imgSrc="http://placehold.it/312x230"
+            title="Easy Strawberry Cake"
+            summary={`Cream Sherry, Moscato d'Asti, and Port are great choices  ...`}
+          />
+        </li>
+      </ul>
     </section>
   );
 }
