7d757de6612c9c08afcd528989928947eda4bef2
hanana1253 <hanana1253@gmail.com>
2022-01-17 15:09:15 +0900

    feat: Link Header login button with Auth component
    - now login dialog available from header button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,36 @@
 import styles from './Header.module.scss';
 import { SearchForm } from '../../components';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Auth } from '../Auth/Auth';
 
 export function Header() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleOpenDialog = () => {
+    setIsVisible(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsVisible(false);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
         <Link to="/">Han Spoon</Link>
         <SearchForm />
       </div>
-      <div>로그인버튼</div>
+      <button
+        type="button"
+        aria-haspopup="dialog"
+        aria-label="Open SignIn Dialog"
+        title="Open SignIn Dialog"
+        onClick={handleOpenDialog}
+      >
+        로그인버튼
+      </button>
+      <Auth isVisible={isVisible} onClose={handleCloseDialog} />
     </header>
   );
 }
