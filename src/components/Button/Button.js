e9b2fcf483cbd5c206ab6814e7a219f8b629bdb5
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-01-17 18:38:07 +0900

    feat: Button Component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,31 @@
-export function Button() {
-  return <button>123</button>;
+import PropTypes from 'prop-types';
+import buttonStyle from './Button.module.scss';
+import classNames from 'classnames';
+
+export function Button({ type, variant, shape, size, color, className, children, ...restProps }) {
+  return (
+    <button
+      type={type}
+      className={classNames(
+        buttonStyle.button,
+        buttonStyle[variant],
+        buttonStyle[shape],
+        buttonStyle[size],
+        buttonStyle[color],
+        className,
+      )}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
 }
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['button', 'submit']).isRequired,
+  variant: PropTypes.oneOf(['text', 'outlined', 'filled']).isRequired,
+  shape: PropTypes.oneOf(['wide', 'round']),
+  size: PropTypes.oneOf(['small', 'large']),
+  color: PropTypes.oneOf(['green', 'orange', 'white']),
+  children: PropTypes.any.isRequired,
+};
