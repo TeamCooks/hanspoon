7c462994951c98e8a6e2c9c7f6c3d62dd782cd95
hanana1253 <hanana1253@gmail.com>
2022-01-19 12:31:43 +0900

    fix: Add PropType check to Heading component
    - I added propTypes to Heading component

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,6 +1,13 @@
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import styles from './Heading.module.scss';
 
 export function Heading({ as: Component, className, children }) {
   return <Component className={classNames(styles.heading, className)}>{children}</Component>;
+}
+
+Heading.propTypes = {
+  as: PropTypes.string.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
+  className: PropTypes.string,
+  children: PropTypes.any.isRequired
 }
