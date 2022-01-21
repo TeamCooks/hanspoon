e2f3a0a8b6474d369c6679855c560f5643d36128
Hangyul <76270892+hanana1253@users.noreply.github.com>
2022-01-21 14:02:31 +0900

    fix: Remove console.log
    - sorry....

--- src/pages/RequireAuth/RequireAuth.js
+++ src/pages/RequireAuth/RequireAuth.js
@@ -3,7 +3,6 @@ import { Navigate } from 'react-router-dom';
 
 export default function RequireAuth({ children }) {
   const authUser = useAuthUser();
-  console.log(authUser);
   if (!authUser) {
     window.alert('Please sign in to have your recipes saved!');
     return <Navigate to="/" replace />;
