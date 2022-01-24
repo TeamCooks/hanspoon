64864e07310048a83b4f3d059863185836331490
hanana1253 <hanana1253@gmail.com>
2022-01-24 21:00:27 +0900

    feat: Make my-recipes page component
    Now available!

--- src/pages/MyRecipes/MyRecipes.js
+++ src/pages/MyRecipes/MyRecipes.js
@@ -1,3 +1,56 @@
+import { getMyRecipes } from '../../api/myRecipes';
+import { useAuthUser } from '../../contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { Loading, Heading, CardList, Pagination } from '../../components';
+import styles from './MyRecipes.module.scss';
+import classNames from 'classnames';
+import { setDocumentTitle } from '../../utils';
+import { Helmet } from 'react-helmet-async';
+
 export default function MyRecipes() {
-  return <div>나의 레시피들이 올 곳입니다.</div>
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const authUser = useAuthUser();
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const data = await getMyRecipes(authUser.uid);
+      setMyRecipes(data);
+    })();
+    setIsLoading(false);
+  }, []);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleClick = (num) => {
+    setCurrentPage(num);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>{setDocumentTitle('My Recipes')}</title>
+      </Helmet>
+      <div className={classNames(styles.container)}>
+        {isLoading ? (
+          <Loading message="Loading my recipes" />
+        ) : isLoading && myRecipes.length === 0 ? (
+          <Pagination.NoResults headline="Nothing saved" description="Save recipes!" />
+        ) : (
+          <>
+            <Heading as="h2" className={styles.heading}>
+              My Recipes
+            </Heading>
+            <CardList
+              results={myRecipes}
+              currentPage={currentPage}
+              totalResults={myRecipes.length}
+              onClick={handleClick}
+            />
+            <Pagination onClick={handleClick} currentPage={currentPage} totalResults={myRecipes.length} />
+          </>
+        )}
+      </div>
+    </>
+  );
 }
