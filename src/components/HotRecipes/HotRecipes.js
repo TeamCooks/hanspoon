301f62084bef2d20392fd682fecfc5cddb1c4d07
joker77z <joker77z@naver.com>
2022-01-26 23:29:06 +0900

    feat:   Add Skeleton Component to Hot Recipe
    스켈레톤 컴포넌트 추가

--- src/components/HotRecipes/HotRecipes.js
+++ src/components/HotRecipes/HotRecipes.js
@@ -2,17 +2,40 @@ import { useEffect, useState } from 'react';
 import { getHotRecipes } from '@api/customApi';
 import { Card } from '../Card/Card';
 import { Heading } from '../Heading/Heading';
+import { SkeletonCard } from '../Card/SkeletonCard';
 
 import styles from './HotRecipes.module.scss';
 
 export function HotRecipes() {
   const [hotRecipes, setHotRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       setHotRecipes(await getHotRecipes());
     })();
+    setTimeout(() => {
+      setLoading(false);
+    }, 1200);
   }, []);
+
+  const renderCards = ({ recipeId, imgSrc, title }) => {
+    if (loading) {
+      return <SkeletonCard type="square" background="none" hasSummary={false} headingPosition="bottomCenter" />;
+    } else {
+      return (
+        <Card
+          id={recipeId}
+          type="square"
+          background="none"
+          hasSummary={false}
+          headingPosition="bottomCenter"
+          imgSrc={imgSrc}
+          title={title}
+        />
+      );
+    }
+  };
 
   return (
     <section className={styles.section}>
@@ -21,18 +44,10 @@ export function HotRecipes() {
       </Heading>
 
       <ul className={styles.cardItems}>
-        {hotRecipes.map(({ recipeId, title, imgSrc }, idx) => {
+        {hotRecipes.map((recipe, idx) => {
           return (
             <li className={styles.item} key={idx}>
-              <Card
-                id={recipeId}
-                type="square"
-                background="none"
-                hasSummary={false}
-                headingPosition="bottomCenter"
-                imgSrc={imgSrc}
-                title={title}
-              />
+              {renderCards(recipe)}
             </li>
           );
         })}
