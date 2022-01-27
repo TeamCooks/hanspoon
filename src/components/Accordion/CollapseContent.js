fa35c22a894b86295f824ae1a038876d70bbe50d
Hyeyeon Seo <skojphy@gmail.com>
2022-01-27 16:23:38 +0900

    feat: Implement sentenceIntoParagraph function
    - 문장 단위로 글을 끊어 <p> 태그로 감싸주는 함수를 구현함.

--- src/components/Accordion/CollapseContent.js
+++ src/components/Accordion/CollapseContent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Accordion.module.scss';
+import { sentenceIntoParagraph } from '@utils';
 
 const CollapseContent = ({ type, content }) => {
   switch (type) {
@@ -13,11 +14,7 @@ const CollapseContent = ({ type, content }) => {
     case 'equipment':
       return content.map((equipment, index) => <li key={'equipment' + index}>{equipment}</li>);
     case 'summary':
-      return content.split('. ').map((text, index, texts) => (
-        <p key={'summary' + index} className={styles.accordionText}>
-          {text + (index < texts.length - 1 ? '.' : '')}
-        </p>
-      ));
+      return sentenceIntoParagraph(content, 'summary', styles.accordionText);
     case 'instructions':
       return content.map((instructions, index) => (
         <li key={'instructions' + index}>
--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,3 +1,10 @@
 export const excludeTags = (content) => content.replace(/<[^>]*>/g, '');
 
 export const camelCase = (data) => data.toString().replace(/\s\w/g, (match) => match.toUpperCase().trim());
+
+export const sentenceIntoParagraph = (content, key, className) =>
+  content.split('. ').map((text, index, texts) => (
+    <p key={key + index} className={className}>
+      {text + (index < texts.length - 1 ? '.' : '')}
+    </p>
+  ));
