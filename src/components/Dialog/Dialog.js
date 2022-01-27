10151432cc3494ffcb86778bc24a6458cb02b735
hanana1253 <hanana1253@gmail.com>
2022-01-27 22:05:57 +0900

    chore: Remove comments and set color to heading of error page and 404 page
    - as written above

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useCallback } from 'react';
-import { string, bool, func, node, oneOf, oneOfType, any } from 'prop-types';
+import { string, func, node, oneOf, oneOfType, any } from 'prop-types';
 import { createPortal } from 'react-dom';
-import { getTabbableElements } from '../../utils';
+import { getTabbableElements } from '@utils';
 import styles from './Dialog.module.scss';
 import classNames from 'classnames';
 import { IconButton } from '../Button/IconButton';
@@ -9,28 +9,21 @@ export function Dialog({ onClose, children, nodeId = 'dialog', label, img, class
   const dialogRef = useRef(null);
   const openButtonRef = useRef(null);
 
-  // 이벤트 리스너
   const handleClose = useCallback(() => {
     onClose();
     openButtonRef.current.focus();
   }, [onClose]);
 
-  // 사이드 이펙트 관리
   useEffect(() => {
-    // 모달 다이얼로그 오프너 버튼 current 값에 참조
     openButtonRef.current = document.activeElement;
 
-    // tabbable 요소들 찾기
     const tabbableElements = getTabbableElements(dialogRef.current);
     const firstTabbableElement = tabbableElements[0];
     const lastTabbableElement = tabbableElements[tabbableElements.length - 1];
 
-    // 첫번째 탭 이동 가능한 요소에 포커싱
     firstTabbableElement.focus();
-    // 이벤트 타입
     let eventType = 'keydown';
 
-    // 이벤트 리스너 정의
     const eventListener = (e) => {
       const { key, shiftKey, target } = e;
 
@@ -49,14 +42,11 @@ export function Dialog({ onClose, children, nodeId = 'dialog', label, img, class
       }
     };
 
-    // 이벤트 구독
     document.addEventListener(eventType, eventListener);
     document.body.style['overflow-y'] = 'hidden';
     document.body.setAttribute('aria-hidden', 'true');
 
-    // 이펙트 클린업(정리) 함수
     return () => {
-      // 이벤트 구독 취소
       document.removeEventListener(eventType, eventListener);
       document.body.removeAttribute('aria-hidden');
       document.body.style['overflow-y'] = '';
