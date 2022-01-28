982eef1da2daf6c127c82347fcc4407751dd11ee
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-01-28 12:47:28 +0900

    fix: Modify canonical for SEO

--- src/components/Meta/Meta.js
+++ src/components/Meta/Meta.js
@@ -13,7 +13,7 @@ export const Meta = ({ data }) => {
   const title = data.title;
   const description = data.description;
   const image = data.image !== undefined && `${data.image}`;
-  const canonical = `https://www.your-homepage.com/${data.canonical}`;
+  const canonical = 'https://hanspoon-31cd9.web.app/';
   const type = data.type === undefined ? 'website' : data.type;
   const width = data.image && (data.width || 1200);
   const height = data.image && (data.height || 630);
